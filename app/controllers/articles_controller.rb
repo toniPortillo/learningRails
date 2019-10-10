b0f438f1477b676887415eb5e83adb9fc6959f84
toniPortillo <toniplsec@gmail.com>
2019-10-11 00:53:37 +0200

    Vista y listar de articulos hecha

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,26 @@
 class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all
+    end
+
     def show 
         @article = Article.find(params[:id])
     end
 
     def new
+        @article = Article.new
     end
 
     # snippet for brevity
-    
+
     def create
         @article = Article.new(article_params)
 
-        @article.save
-        redirect_to @article
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end
     end
 
     private
